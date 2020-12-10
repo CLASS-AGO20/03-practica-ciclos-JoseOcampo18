aac5035057f06c6a0f9b84719245389a6c7bd2fd
Jose <jocampo2@ucol.mx>
2020-12-10 16:34:15 -0600

    Implemented 3 functions, no pude hacer los commit de uno en uno por que la verdad no estoy en mi casa y estoy utilizando datos, una disculpa

--- src/main.js
+++ src/main.js
@@ -8,6 +8,41 @@ export default class App {
         return multiplicacion;
     }
 
+    convertirAString(numero){
+            let cadena ="";
+            let i = 1;
+
+            while (i <= numero){
+                    cadena += "*"; 
+                    i++;
+            }
+            
+            return cadena;
+    }
+
+    obtenerDivisibles(numero) {
+        let i = 1;
+        let sum = 0;
+        do {
+          if (numero % i == 0) {
+            sum = sum + 1;
+            i++;
+          } else {
+            i++;
+          }
+        } while (i <= numero);
+        return sum;
+      }
+
+      elevar(numero, potencia) {
+        let suma = 1;
+        for (let i = 1; i <= potencia; i++) {
+          suma = suma * numero;
+        }
+        return suma;
+      }
+
 }
 
 let app = new App();
+console.log(app.convertirAString(5));
